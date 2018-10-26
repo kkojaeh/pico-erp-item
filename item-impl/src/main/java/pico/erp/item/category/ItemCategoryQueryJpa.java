42e414cf422b500843afc473ed06f9720f7aa454
Ethan <kkojaeh@gmail.com>
2018-10-26 10:57:24 +0900

    UUID 프로젝션 문제 수정

--- item-impl/src/main/java/pico/erp/item/category/ItemCategoryQueryJpa.java
+++ item-impl/src/main/java/pico/erp/item/category/ItemCategoryQueryJpa.java
@@ -13,9 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
-import pico.erp.shared.ExtendedLabeledValue;
 import pico.erp.shared.LabeledValue;
 import pico.erp.shared.Public;
+import pico.erp.shared.QExtendedLabeledValue;
 import pico.erp.shared.data.LabeledValuable;
 import pico.erp.shared.jpa.QueryDslJpaSupport;
 
@@ -36,7 +36,7 @@ public class ItemCategoryQueryJpa implements ItemCategoryQuery {
   @Override
   public List<? extends LabeledValuable> asLabels(String keyword, long limit) {
     val query = new JPAQuery<LabeledValue>(entityManager);
-    val select = Projections.bean(ExtendedLabeledValue.class,
+    val select = new QExtendedLabeledValue(
       itemCategory.id.value.as("value"),
       itemCategory.name.as("label"),
       itemCategory.path.as("subLabel"),
