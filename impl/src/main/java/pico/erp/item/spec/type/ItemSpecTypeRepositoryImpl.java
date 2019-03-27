41b2defb2de04bc63ffaabfa4cb132abee3b2a8a
Ethan <kkojaeh@gmail.com>
2019-03-27 16:30:58 +0900

    spring boot 2 반영

--- impl/src/main/java/pico/erp/item/spec/type/ItemSpecTypeRepositoryImpl.java
+++ impl/src/main/java/pico/erp/item/spec/type/ItemSpecTypeRepositoryImpl.java
@@ -1,6 +1,5 @@
 package pico.erp.item.spec.type;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
@@ -23,12 +22,6 @@ public class ItemSpecTypeRepositoryImpl implements ItemSpecTypeRepository {
     return types.stream()
       .filter(type -> type.getId().equals(id))
       .findFirst();
-  }
-
-  public static void main(String... args) {
-    System.out.println(LocalDateTime.now());
-    System.out.println(LocalDateTime.parse("2019-10-18T00:00:00"));
-    ;
   }
 
 }
